64e014df317a0837aa92051d134b9ff4aba07b7b
Jorge <jorgeluispenate@gmail.com>
2023-10-11 21:51:52 +0100

    Cambiado el tres en raya a una nueva pantalla Se ha cambiado el código del tres en raya de MainActivity a PantallaTresEnRaya

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,94 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import androidx.appcompat.app.AlertDialog
-import androidx.core.view.get
-import com.example.myapplication.R
-import com.example.myapplication.GestorTablero
 
 class MainActivity : AppCompatActivity() {
-    private val gameController = GestorTablero()
-    private lateinit var tablero: Tablero
-    private var end = false
-    private var turnoJugador1 = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        inicializarPartida()
-        inicializarSensor()
-    }
+        val btn: Button = findViewById(R.id.butnplay)
+        btn.setOnClickListener{
 
-    private fun inicializarPartida(){
-        tablero = gameController.nuevoTablero()
-        end = false
-        turnoJugador1 = true
-        val tableroLayout = findViewById<LinearLayout>(R.id.tableroLayout)
-        for (i in 0 until tableroLayout.childCount) {
-            val fila = tableroLayout[i] as LinearLayout
-            for (j in 0 until fila.childCount) {
-                val ficha = fila[j] as ImageView
-                ficha.setImageDrawable(getDrawable(R.drawable.cuadrado))
-            }
+            val intent: Intent = Intent(this,PantallaTresEnRaya::class.java)
+            startActivity(intent)
         }
     }
-
-    private fun inicializarSensor(){
-        val reiniciar = findViewById<Button>(R.id.reiniciar)
-        reiniciar.setOnClickListener(){
-            inicializarPartida()
-        }
-        val tableroLayout = findViewById<LinearLayout>(R.id.tableroLayout)
-        for(i in 0 until tableroLayout.childCount){
-            val fila = tableroLayout[i] as LinearLayout
-            for (j in 0 until fila.childCount){
-                val ficha = fila[j] as ImageView
-                ficha.setOnClickListener{
-                    if(!end && tablero.isEmpty(i,j)) {
-                        setFicha(ficha, i, j)
-                        val estadoJuego = gameController.estadoPartida(turnoJugador1)
-                        if(turnoJugador1 && estadoJuego == GestorTablero.PartidaState.GanaJug1){
-                            end = true
-                            mostrarMensaje("Ganó el jugador 1")
-                        }else if(!turnoJugador1 && estadoJuego == GestorTablero.PartidaState.GanaJug2){
-                            end = true
-                            mostrarMensaje("Ganó el jugador 2")
-                        } else if(estadoJuego == GestorTablero.PartidaState.Empate){
-                            end = true
-                            mostrarMensaje("El resultado es un empate")
-                        } else{
-                            turnoJugador1 = !turnoJugador1
-                        }
-                    }
-                }
-            }
-
-        }
-    }
-
-    private fun mostrarMensaje(mensaje: String){
-        AlertDialog.Builder(this)
-            .setTitle(mensaje)
-            .setPositiveButton("Jugar de nuevo", { dialog, which -> inicializarPartida() })
-            .setNegativeButton("Cancelar", { dialog, which -> dialog.dismiss() })
-            .show()
-    }
-
-    private fun setFicha(view: ImageView, posFila: Int, posCol: Int){
-        if(turnoJugador1) {
-            tablero.markCircle(posFila, posCol)
-            view.setImageDrawable(getDrawable(R.drawable.circulo))
-
-        }
-        else{
-            tablero.markCross(posFila, posCol)
-            view.setImageDrawable(getDrawable(R.drawable.cruz))
-        }
-    }
-
-
 }
---
+++ app/src/main/java/com/example/myapplication/PantallaTresEnRaya.kt
@@ -0,0 +1,102 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
+import com.example.myapplication.R
+import com.example.myapplication.GestorTablero
+
+class PantallaTresEnRaya : AppCompatActivity() {
+    private val gameController = GestorTablero()
+    private lateinit var tablero: Tablero
+    private var end = false
+    private var turnoJugador1 = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_tres_en_raya)
+
+        inicializarPartida()
+        inicializarSensor()
+
+        val btnvolver: Button = findViewById(R.id.volver)
+        btnvolver.setOnClickListener{
+
+            val intent: Intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun inicializarPartida(){
+        tablero = gameController.nuevoTablero()
+        end = false
+        turnoJugador1 = true
+        val tableroLayout = findViewById<LinearLayout>(R.id.tableroLayout)
+        for (i in 0 until tableroLayout.childCount) {
+            val fila = tableroLayout[i] as LinearLayout
+            for (j in 0 until fila.childCount) {
+                val ficha = fila[j] as ImageView
+                ficha.setImageDrawable(getDrawable(R.drawable.cuadrado))
+            }
+        }
+    }
+
+    private fun inicializarSensor(){
+        val reiniciar = findViewById<Button>(R.id.reiniciar)
+        reiniciar.setOnClickListener(){
+            inicializarPartida()
+        }
+        val tableroLayout = findViewById<LinearLayout>(R.id.tableroLayout)
+        for(i in 0 until tableroLayout.childCount){
+            val fila = tableroLayout[i] as LinearLayout
+            for (j in 0 until fila.childCount){
+                val ficha = fila[j] as ImageView
+                ficha.setOnClickListener{
+                    if(!end && tablero.isEmpty(i,j)) {
+                        setFicha(ficha, i, j)
+                        val estadoJuego = gameController.estadoPartida(turnoJugador1)
+                        if(turnoJugador1 && estadoJuego == GestorTablero.PartidaState.GanaJug1){
+                            end = true
+                            mostrarMensaje("Ganó el jugador 1")
+                        }else if(!turnoJugador1 && estadoJuego == GestorTablero.PartidaState.GanaJug2){
+                            end = true
+                            mostrarMensaje("Ganó el jugador 2")
+                        } else if(estadoJuego == GestorTablero.PartidaState.Empate){
+                            end = true
+                            mostrarMensaje("El resultado es un empate")
+                        } else{
+                            turnoJugador1 = !turnoJugador1
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun mostrarMensaje(mensaje: String){
+        AlertDialog.Builder(this)
+            .setTitle(mensaje)
+            .setPositiveButton("Jugar de nuevo", { dialog, which -> inicializarPartida() })
+            .setNegativeButton("Cancelar", { dialog, which -> dialog.dismiss() })
+            .show()
+    }
+
+    private fun setFicha(view: ImageView, posFila: Int, posCol: Int){
+        if(turnoJugador1) {
+            tablero.markCircle(posFila, posCol)
+            view.setImageDrawable(getDrawable(R.drawable.circulo))
+
+        }
+        else{
+            tablero.markCross(posFila, posCol)
+            view.setImageDrawable(getDrawable(R.drawable.cruz))
+        }
+    }
+
+
+}

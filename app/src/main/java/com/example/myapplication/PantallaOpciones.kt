37b632215fdeb4312e93a762d2099cc274e97d6d
josrob13 <ja.villagran.2021@alumnos.urjc.es>
2023-11-14 12:44:41 +0100

    Configuración Actualizada

--- app/src/main/java/com/example/myapplication/PantallaOpciones.kt
+++ app/src/main/java/com/example/myapplication/PantallaOpciones.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class PantallaOpciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,8 @@ class PantallaOpciones : AppCompatActivity() {
             val intent1 = Intent(this,MainMenu::class.java)
             startActivity(intent1)
         }
+
+        val titulo: TextView = findViewById(R.id.titulo)
+        titulo.paintFlags = titulo.paintFlags or Paint.UNDERLINE_TEXT_FLAG
     }
 }

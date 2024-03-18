0ff98463e037fc06a450c15a9d263eac737cef19
Daniel <danigomezlopez@gmail.com>
2024-03-18 12:05:59 +0100

    Revert "bug3"
    This reverts commit 2b30be362d3a7e45b7537c897c0b9c6a244c1a04.

--- app/src/main/java/com/example/myapplication/SeleccionModo.kt
+++ app/src/main/java/com/example/myapplication/SeleccionModo.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class pantalla_seleccion_modo : AppCompatActivity() {
+class SeleccionModo : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

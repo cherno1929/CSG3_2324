085851d580816a2f22e18e152de85e2b4e80ad06
magarciam2003 <ma.garciam.2021@alumnos.urjc.es>
2024-05-04 22:03:35 +0200

    dependency change

--- app/src/androidTest/java/com/example/myapplication/CucumberSetup.kt
+++ app/src/androidTest/java/com/example/myapplication/CucumberSetup.kt
@@ -1,8 +1,8 @@
 package com.example.myapplication
 
+
 import cucumber.api.CucumberOptions
 import cucumber.api.junit.Cucumber
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
